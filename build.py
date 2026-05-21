99803e900a1a0d60494614387fa6355ee743d383
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-05-21 22:18:01 +0200

    Add OWL generator

--- build.py
+++ build.py
@@ -8,6 +8,8 @@
 # ]
 # ///
 
+import os
+import pandas as pd
 from pydantic import BaseModel
 import pystow
 from tabulate import tabulate
@@ -29,6 +31,14 @@ class BasisSet(BaseModel):
     description: str
     family: str
     function_types: list[str]
+
+
+NAMES = {
+    "gto": "Gaussian-type orbitals",
+    "gto_spherical": "spherical Gaussian-type orbitals",
+    "gto_cartesian": "cartesian Gaussian-type orbitals",
+    "scalar_ecp": "scalar effective core potentials",
+}
 
 
 @click.command()
@@ -54,14 +64,81 @@ def main() -> None:
         for function_type in basis_set.function_types:
             function_type_counter[function_type] += 1
 
+    prefix = "OTC"
+    header_1 = ("identifier", "type", "label", "abbreviation", "parent")
+    header_2 = (
+        "ID",
+        "TYPE",
+        "AT rdfs:label^^xsd:string",
+        "AT oboInOwl:hasExactSynonym^^xsd:string",
+        "SC %",
+    )
+    rows = [
+        header_2,
+        (f"{prefix}:0000001", "owl:Class", "role", "", ""),
+        (f"{prefix}:0000002", "owl:Class", "family", "", ""),
+        (f"{prefix}:0000003", "owl:Class", "function type", "", ""),
+        (f"{prefix}:0000004", "owl:Class", "basis set", "", ""),
+    ]
     click.echo(
         f"Roles:\n\n{tabulate(role_counter.most_common(), headers=['role', 'count'], tablefmt='github')}"
     )
+    counter = 5
+    for role in role_counter:
+        rows.append(
+            (
+                f"{prefix}:{counter:07}",
+                "owl:Class",
+                NAMES.get(role),
+                role,
+                f"{prefix}:0000001",
+            )
+        )
+        counter += 1
     click.echo(
         f"\n\nFamilies:\n\n{tabulate(family_counter.most_common(), headers=['family', 'count'], tablefmt='github')}"
     )
+    for family in family_counter:
+        rows.append(
+            (
+                f"{prefix}:{counter:07}",
+                "owl:Class",
+                NAMES.get(family),
+                family,
+                f"{prefix}:0000002",
+            )
+        )
+        counter += 1
     click.echo(
         f"\n\nFunction Types:\n\n{tabulate(function_type_counter.most_common(), headers=['function type', 'count'], tablefmt='github')}"
+    )
+    for function_type in function_type_counter:
+        rows.append(
+            (
+                f"{prefix}:{counter:07}",
+                "owl:Class",
+                NAMES.get(function_type),
+                function_type,
+                f"{prefix}:0000003",
+            )
+        )
+        counter += 1
+
+    for basis_set in basis_sets:
+        rows.append(
+            (
+                f"{prefix}:{counter:07}",
+                "owl:Class",
+                NAMES.get(basis_set.name),
+                basis_set.name,
+                f"{prefix}:0000004",
+            )
+        )
+        counter += 1
+
+    pd.DataFrame(rows, columns=header_1).to_csv("terms.tsv", sep="\t", index=False)
+    os.system(
+        'robot template --prefix "OTC: http://purl.obolibrary.org/obo/OTC_" --template terms.tsv --output otc.owl'
     )
 
 
